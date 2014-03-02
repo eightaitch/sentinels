ec22500a1097a4d43bde7b175f849df51118f27b
anon <anon@anon-VirtualBox.(none)>
2014-03-02 12:13:38 -0700

    first react componenets

--- index.jsx
+++ index.jsx
@@ -1,1 +1,9 @@
-console.log('sentinels');
+/** @jsx React.DOM */
+
+var React = require('react');
+var Board = require('./src/Board.jsx');
+
+React.renderComponent(
+    <Board />,
+    document.body
+);
