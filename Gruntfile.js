59b7948fa7f4c80f6e04cfa01fed565066b8b25f
anon <anon@anon-VirtualBox.(none)>
2014-03-02 11:47:34 -0700

    grunt-react instead of reactify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,15 @@
 module.exports = function(grunt) {
 
+    var pkg = grunt.file.readJSON('package.json')
+        , dev = pkg.devDependencies;
+
+    // Automatically load grunt-* npm tasks
+    grunt.util._.each(dev, function (ver, name) {
+        if (/^grunt-/.test(name)) {
+            grunt.loadNpmTasks(name);
+        }
+    });
+
     grunt.registerTask('log', 'Log some stuff.', function() {
         grunt.log.write('Logging some stuff . . .').ok();
     });
