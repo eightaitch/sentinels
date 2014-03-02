153798ea6adb5e258f59280ad0a2dfe659f0e9a0
anon <anon@anon-VirtualBox.(none)>
2014-03-02 11:15:18 -0700

    updated hero with new card format

--- data/heroes/TheWraith.js
+++ data/heroes/TheWraith.js
@@ -1,34 +1,34 @@
-var TheWraith = {
-    hero: {
-        name: 'The Wraith',
-        hp: 26,
-        power: {
-            name: 'Stealth',
-            text: 'Reduce the next damage that would be dealth to The Wraith by 2.'
-        }
-        back: [
-            'One player may play a card now.',
-            'One hero regains 2 hp.',
-            'Destroy an ongoing card.'
-        ]
-    },
-    cards: [
-        {
-            card: {
-                name: 'Combat Stance',
-                type: ['ongoing', 'limited'],
-                text: 'The first time The Wraith is dealth damage by a target each turn, The Wraith deals that target 2 melee damage.'
-            },
-            count: 2
+var TheWraith = [
+    {
+        front: {
+            name: 'The Wraith',
+            hp: 26,
+            power: {
+                name: 'Stealth',
+                text: 'Reduce the next damage that would be dealth to The Wraith by 2.'
+            }
         },
-        {
-            card: {
-                name: 'Grappling Hook',
-                type: ['one-shot'],
-                text: 'Detstroy 1 ongoing or environment card.\nYou may draw a dard.'
-            },
-            count: 3
-        }
-    ]
-}
+        back: {
+            name: 'The Wraith',
+            text: 'One player may play a card now.\nOne hero regains 2 hp.\nDestroy an ongoing card.'
+        },
+        count: 1
+    },
+    {
+        front: {
+            name: 'Combat Stance',
+            type: ['ongoing', 'limited'],
+            text: 'The first time The Wraith is dealth damage by a target each turn, The Wraith deals that target 2 melee damage.'
+        },
+        count: 2
+    },
+    {
+        front: {
+            name: 'Grappling Hook',
+            type: ['one-shot'],
+            text: 'Detstroy 1 ongoing or environment card.\nYou may draw a dard.'
+        },
+        count: 3
+    }
+]
 
